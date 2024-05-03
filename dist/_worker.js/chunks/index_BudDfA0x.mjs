6ff86c8c5c5dad8e319824c259d59a49aaf77722
Nafeez <nafeez@cloudflare.com>
2024-05-02 20:42:48 -0700

    cache builds, this is an experiment.. look away

---
+++ dist/_worker.js/chunks/index_BudDfA0x.mjs
@@ -0,0 +1,6 @@
+globalThis.process ??= {}; globalThis.process.env ??= {};
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_kgAbO2YY.mjs');
+
+export { page };
