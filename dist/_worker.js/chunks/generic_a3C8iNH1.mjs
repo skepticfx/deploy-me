6ab9c3f849d18b629c5c0bd5a98f68f975081ebc
Nafeez <nafeez@cloudflare.com>
2024-05-02 21:16:48 -0700

    entry

--- dist/_worker.js/chunks/generic_a3C8iNH1.mjs
+++
@@ -1,6 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_BgvxNCG6.mjs');
-
-export { page };
--- dist/_worker.js/chunks/index_BudDfA0x.mjs
+++
@@ -1,6 +0,0 @@
-globalThis.process ??= {}; globalThis.process.env ??= {};
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/index_kgAbO2YY.mjs');
-
-export { page };
